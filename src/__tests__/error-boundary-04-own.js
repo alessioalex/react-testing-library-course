e8d96d6fb7022585e0bee4e57588b69076af8e87
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-30 00:54:52 +0300

    feat(wrapper):simplify ErrorBoundary code with wrapper option

---
+++ src/__tests__/error-boundary-04-own.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import {ErrorBoundary} from '../error-boundary'
+import {reportError as mockReportError} from '../api'
+
+jest.mock('../api')
+
+function Bomb({shouldThrow}) {
+  if (shouldThrow) {
+    // console.error('really really bad do not miss this message')
+    throw new Error('boom')
+  } else {
+    return null
+  }
+}
+
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterAll(() => {
+  console.error.mockRestore()
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('calls reportError and render that there was a problem', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+
+  const {
+    rerender,
+    getByText,
+    getByRole,
+    queryByRole,
+    queryByText,
+  } = render(<Bomb shouldThrow={true} />, {wrapper: ErrorBoundary})
+
+  rerender(<Bomb shouldThrow={true} />)
+
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+
+  expect(console.error).toHaveBeenCalledTimes(2)
+
+  expect(getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"There was a problem."`,
+  )
+
+  console.error.mockClear()
+  mockReportError.mockClear()
+
+  rerender(<Bomb />)
+
+  fireEvent.click(getByText(/try again/i))
+
+  expect(mockReportError).not.toHaveBeenCalled()
+  expect(console.error).not.toHaveBeenCalled()
+  expect(queryByRole('alert')).not.toBeInTheDocument()
+  expect(queryByText(/try again/i)).not.toBeInTheDocument()
+})
