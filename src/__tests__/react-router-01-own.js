37dd55411969d340c58647e2e70e06c827fb24e4
Alexandru Vladutu <github@social.alessioalex.com>
2020-05-02 00:40:13 +0300

    initialize the history object with a bad entry to test the react router no match route

--- src/__tests__/react-router-01-own.js
+++ src/__tests__/react-router-01-own.js
@@ -4,17 +4,29 @@ import {createMemoryHistory} from 'history'
 import {render, fireEvent} from '@testing-library/react'
 import {Main} from '../main'
 
-test('main renders about and home and I can navigate to those pages', () => {
-  const history = createMemoryHistory({initialEntries: ['/']})
-  const {getByRole, getByText, debug} = render(
+function renderMain({initialEntries} = {initialEntries: ['/']}) {
+  const history = createMemoryHistory({initialEntries})
+  const utils = render(
     <Router history={history}>
       <Main />
     </Router>,
   )
 
+  return {...utils}
+}
+
+test('main renders about and home and I can navigate to those pages', () => {
+  const {getByRole, getByText} = renderMain()
+
   expect(getByRole('heading')).toHaveTextContent(/home/i)
-  debug()
   fireEvent.click(getByText(/about/i))
-  debug()
   expect(getByRole('heading')).toHaveTextContent(/about/i)
+})
+
+test('landing on a bad page shows a no match component', () => {
+  const {getByRole} = renderMain({
+    initialEntries: ['/something-that-does-not-match'],
+  })
+
+  expect(getByRole('heading')).toHaveTextContent(/404/i)
 })
