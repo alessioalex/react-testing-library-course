6f357083f1f9f9c5df4017f21e9d861cc5b09eb2
Alexandru Vladutu <github@social.alessioalex.com>
2020-05-02 01:07:11 +0300

    test a redux connected react component with initialized state

--- src/__tests__/redux-01-own.js
+++ src/__tests__/redux-01-own.js
@@ -2,15 +2,30 @@ import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
 import {Provider} from 'react-redux'
 import {Counter} from '../redux-counter'
-import {store} from '../redux-store'
+import {store as appStore} from '../redux-store'
+import {createStore} from 'redux'
+import {reducer} from '../redux-reducer'
 
 test('can render with redux with defaults', () => {
   const {getByText, getByLabelText} = render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>,
   )
 
   fireEvent.click(getByText('+'))
   expect(getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redux with custom state', () => {
+  const store = createStore(reducer, {count: 3})
+
+  const {getByText, getByLabelText} = render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+
+  fireEvent.click(getByText('-'))
+  expect(getByLabelText(/count/i)).toHaveTextContent('2')
 })
